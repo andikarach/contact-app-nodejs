8e75e13354250249621abf15ef7c75678cbbe6d1
andikarach <andika.rach4@gmail.com>
2021-05-19 11:16:07 +0700

    add gitnignore, change port

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
