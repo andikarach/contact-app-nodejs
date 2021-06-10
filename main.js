60f0012401a2ae5ee7716d50931a3d2768d4512f
andikarach <60335830+andikarach@users.noreply.github.com>
2021-06-10 16:22:12 +0700

    change port

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
